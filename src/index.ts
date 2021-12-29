dd4b0e210ce3a7d9d89e3fe9413150c93ae885e1
Erik Carlson <carlsonerikw@gmail.com>
2021-12-29 14:24:50 -0500

    Remove old index.tsx in favor of exporting Editor component and its types. Move reset.css into Container styled component.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/* -------- Types -------- */
+import { EditorProps } from './editor'
+/* -------- Components -------- */
+import Editor from './editor';
+
+export type {
+  EditorProps
+}
+
+export {
+  Editor
+}
