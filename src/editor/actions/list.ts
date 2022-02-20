f4e642f3f1240f9e9b914b45d2c2753784bf180e
Erik Carlson <carlsonerikw@gmail.com>
2022-02-20 18:04:41 -0500

    Maintain focus when in and outdenting a list item regardless of inline element or not.

--- src/editor/actions/list.ts
+++ src/editor/actions/list.ts
@@ -1,21 +1,43 @@
 import { Transforms, Path, Descendant } from "slate";
-import { getElementPath, getElementNode, getParentElementNode } from "./element";
-import { CustomEditor, CustomElement, ListElement, ListElementType, ListItemElement } from "../types";
+import {
+  getElementPath,
+  getElementNode,
+  getParentElementNode,
+} from "./element";
+import {
+  CustomEditor,
+  ListElement,
+  ListElementType,
+  ListItemElement,
+} from "../types";
 import { focusPath } from "./utils";
-import { getContainer, getContainerParent, getContainerPath, isInlineActive } from "./inline";
+import {
+  getContainer,
+  getContainerParent,
+  getContainerPath,
+  isInlineActive,
+} from "./inline";
 
 const isListElement = (element: Descendant | null): element is ListElement => {
-  return element && "type" in element && (element.type === 'bulleted-list' || element.type === 'ordered-list') ? true : false;
-}
+  return element &&
+    "type" in element &&
+    (element.type === "bulleted-list" || element.type === "ordered-list")
+    ? true
+    : false;
+};
 
-const mergeWithPreviousList = (editor: CustomEditor, listPath: Path, listItemNode: ListItemElement, listNode: ListElement) => {
+const mergeWithPreviousList = (
+  editor: CustomEditor,
+  listPath: Path,
+  listItemNode: ListItemElement,
+  listNode: ListElement,
+  finalFocusPath: Path,
+  finalFocusOffset: number
+) => {
   // Merge list item with the previous list
   const mergedNode: ListElement = {
     type: listNode.type,
-    children: [
-      ...listNode.children,
-      listItemNode,
-    ]
+    children: [...listNode.children, listItemNode],
   };
   // Remove old nodes
   Transforms.removeNodes(editor, { at: listPath });
@@ -23,17 +45,22 @@ const mergeWithPreviousList = (editor: CustomEditor, listPath: Path, listItemNod
   // Insert new node
   Transforms.insertNodes(editor, mergedNode, { at: listPath });
   // Focus correct location in new merged node
-  focusPath(editor, [...listPath, listNode.children.length]);
+  console.log('editor.children:', editor.children, [...listPath, listNode.children.length, ...finalFocusPath]);
+  focusPath(editor, [...listPath, listNode.children.length, ...finalFocusPath], finalFocusOffset);
 };
 
-const mergeWithNextList = (editor: CustomEditor, listItemPath: Path, listItemNode: ListItemElement, listNode: ListElement) => {
+const mergeWithNextList = (
+  editor: CustomEditor,
+  listItemPath: Path,
+  listItemNode: ListItemElement,
+  listNode: ListElement,
+  finalFocusPath: Path,
+  finalFocusOffset: number
+) => {
   // Merge list item with the following list
   const mergedNode: ListElement = {
     type: listNode.type,
-    children: [
-      listItemNode,
-      ...listNode.children
-    ]
+    children: [listItemNode, ...listNode.children],
   };
   // Remove old nodes
   Transforms.removeNodes(editor, { at: listItemPath });
@@ -41,18 +68,23 @@ const mergeWithNextList = (editor: CustomEditor, listItemPath: Path, listItemNod
   // Insert new node
   Transforms.insertNodes(editor, mergedNode, { at: listItemPath });
   // Focus correct location in new merged node
-  focusPath(editor, [...listItemPath, 0]);
+  console.log('mergewithnext focus - ', [...listItemPath, 0, ...finalFocusPath], finalFocusOffset, editor.children);
+  focusPath(editor, [...listItemPath, 0, ...finalFocusPath], finalFocusOffset);
 };
 
-const mergeTwoLists = (editor: CustomEditor, currentListItem: ListItemElement, prevListPath: Path, prevList: ListElement, nextList: ListElement) => {
+const mergeTwoLists = (
+  editor: CustomEditor,
+  currentListItem: ListItemElement,
+  prevListPath: Path,
+  prevList: ListElement,
+  nextList: ListElement,
+  finalFocusPath: Path,
+  finalFocusOffset: number
+) => {
   // Construct node merging previous, current and next nodes
   const mergedNode: ListElement = {
     type: prevList.type,
-    children: [
-      ...prevList.children,
-      currentListItem,
-      ...nextList.children
-    ]
+    children: [...prevList.children, currentListItem, ...nextList.children],
   };
   // Remove old nodes
   Transforms.removeNodes(editor, { at: prevListPath });
@@ -61,35 +93,64 @@ const mergeTwoLists = (editor: CustomEditor, currentListItem: ListItemElement, p
   // Insert new node
   Transforms.insertNodes(editor, mergedNode, { at: prevListPath });
   // Focus correct location in new merged node
-  focusPath(editor, [...prevListPath, prevList.children.length]);
-}
+  focusPath(editor, [...prevListPath, prevList.children.length, ...finalFocusPath], finalFocusOffset);
+};
 
-const wrapListItem = (editor: CustomEditor, path: Path, node: ListItemElement, listType: ListElementType) => {
+const wrapListItem = (
+  editor: CustomEditor,
+  path: Path,
+  node: ListItemElement,
+  listType: ListElementType,
+  finalFocusPath: Path,
+  finalFocusOffset: number
+) => {
   // Wrap current list item node with a list node
   const listNode: ListElement = {
     type: listType,
-    children: [
-      node
-    ]
+    children: [node],
   };
   Transforms.removeNodes(editor, { at: path });
-  Transforms.insertNodes(editor, listNode, { at: path }); 
-  focusPath(editor, path);
+  Transforms.insertNodes(editor, listNode, { at: path });
+  console.log('editor.children:', editor.children, [...path, ...finalFocusPath]);
+  focusPath(editor, [...path, 0, ...finalFocusPath], finalFocusOffset );
 };
 
 const indentListItem = (editor: CustomEditor) => {
   const activeInline = isInlineActive(editor);
-  const currentPath = activeInline ? getContainerPath(editor) : getElementPath(editor);
-  const currentNode = activeInline ? getContainer(editor) : getElementNode(editor);
-  const parentNode = activeInline ? getContainerParent(editor): getParentElementNode(editor);
-  if (currentNode && currentNode.type === 'list-item' && currentPath && parentNode && isListElement(parentNode)) {
+  const currentPath = activeInline
+    ? getContainerPath(editor)
+    : getElementPath(editor);
+  const currentNode = activeInline
+    ? getContainer(editor)
+    : getElementNode(editor);
+  const parentNode = activeInline
+    ? getContainerParent(editor)
+    : getParentElementNode(editor);
+  if (
+    editor.selection && 
+    currentNode &&
+    currentNode.type === "list-item" &&
+    currentPath &&
+    parentNode &&
+    isListElement(parentNode)
+  ) {
     // can't indent if there is only one item in the list
     if (parentNode.children.length === 1) return;
 
     // Get the current node's index (for parentNode's children)
-    const currentNodeIndex = currentPath[currentPath.length - 1];
+    const currentNodeIndex = currentPath[currentPath.length - 1]
 
-    if (parentNode.children.length >= 3 && (currentNodeIndex > 0 && currentNodeIndex < parentNode.children.length - 1)) {
+    const selectionPath = editor.selection.anchor.path;
+    const finalFocusPath = activeInline
+      ? selectionPath.slice(selectionPath.length - 2)
+      : selectionPath.slice(selectionPath.length - 1);
+    const finalfocusOffset = editor.selection.anchor.offset;
+
+    if (
+      parentNode.children.length >= 3 &&
+      currentNodeIndex > 0 &&
+      currentNodeIndex < parentNode.children.length - 1
+    ) {
       // Create Path to the node before the current node
       const prevNodePath: Path = [...currentPath];
       prevNodePath[prevNodePath.length - 1] -= 1;
@@ -103,13 +164,21 @@ const indentListItem = (editor: CustomEditor) => {
       const prevNodeItem = getElementNode(editor, prevNodePath);
 
       if (isListElement(nextNodeItem) && isListElement(prevNodeItem)) {
-        mergeTwoLists(editor, currentNode, prevNodePath, prevNodeItem, nextNodeItem)
+        mergeTwoLists(
+          editor,
+          currentNode,
+          prevNodePath,
+          prevNodeItem,
+          nextNodeItem,
+          finalFocusPath,
+          finalfocusOffset
+        );
       } else if (isListElement(nextNodeItem)) {
-        mergeWithNextList(editor, currentPath, currentNode, nextNodeItem);
+        mergeWithNextList(editor, currentPath, currentNode, nextNodeItem, finalFocusPath, finalfocusOffset);
       } else if (isListElement(prevNodeItem)) {
-        mergeWithPreviousList(editor, prevNodePath, currentNode, prevNodeItem);
+        mergeWithPreviousList(editor, prevNodePath, currentNode, prevNodeItem, finalFocusPath, finalfocusOffset);
       } else {
-        wrapListItem(editor, currentPath, currentNode, parentNode.type);
+        wrapListItem(editor, currentPath, currentNode, parentNode.type, finalFocusPath, finalfocusOffset);
       }
     } else if (currentNodeIndex === 0) {
       // Create Path to the node after the current node
@@ -119,9 +188,9 @@ const indentListItem = (editor: CustomEditor) => {
       const nextNodeItem = getElementNode(editor, nextNodePath);
 
       if (isListElement(nextNodeItem)) {
-        mergeWithNextList(editor, currentPath, currentNode, nextNodeItem);
+        mergeWithNextList(editor, currentPath, currentNode, nextNodeItem, finalFocusPath, finalfocusOffset);
       } else {
-        wrapListItem(editor, currentPath, currentNode, parentNode.type);
+        wrapListItem(editor, currentPath, currentNode, parentNode.type, finalFocusPath, finalfocusOffset);
       }
     } else if (currentNodeIndex === parentNode.children.length - 1) {
       // Create Path to the node before the current node
@@ -131,9 +200,9 @@ const indentListItem = (editor: CustomEditor) => {
       const prevNodeItem = getElementNode(editor, prevNodePath);
 
       if (isListElement(prevNodeItem)) {
-        mergeWithPreviousList(editor, prevNodePath, currentNode, prevNodeItem);
+        mergeWithPreviousList(editor, prevNodePath, currentNode, prevNodeItem, finalFocusPath, finalfocusOffset);
       } else {
-        wrapListItem(editor, currentPath, currentNode, parentNode.type);
+        wrapListItem(editor, currentPath, currentNode, parentNode.type, finalFocusPath, finalfocusOffset);
       }
     }
   }
@@ -141,112 +210,155 @@ const indentListItem = (editor: CustomEditor) => {
 
 const outdentListItem = (editor: CustomEditor) => {
   const activeInline = isInlineActive(editor);
-  const currentPath = activeInline ? getContainerPath(editor) : getElementPath(editor);
-  const currentNode = activeInline ? getContainer(editor) : getElementNode(editor);
-  const parentNode = activeInline ? getContainerParent(editor): getParentElementNode(editor);
-  if (parentNode && isListElement(parentNode) && currentNode && currentPath && currentPath.length > 2) {
+  const currentPath = activeInline
+    ? getContainerPath(editor)
+    : getElementPath(editor);
+  const currentNode = activeInline
+    ? getContainer(editor)
+    : getElementNode(editor);
+  const parentNode = activeInline
+    ? getContainerParent(editor)
+    : getParentElementNode(editor);
+  if (
+    editor.selection &&
+    parentNode &&
+    isListElement(parentNode) &&
+    currentNode &&
+    currentPath &&
+    currentPath.length > 2
+  ) {
     let insertPath = [...currentPath].slice(0, currentPath.length - 1);
     const currentNodeIndex = currentPath[currentPath.length - 1];
+    const selectionPath = editor.selection.anchor.path;
+    const finalFocusPath = activeInline
+      ? selectionPath.slice(selectionPath.length - 2)
+      : selectionPath.slice(selectionPath.length - 1);
+    const focusOffset = editor.selection.anchor.offset;
     if (parentNode.children.length === 1) {
       /*
-      * case: The user unindents the last item in a list. The list item's parent node should be 
-      *       removed and this list item should be inserted in its place.
-      */
+       * case: The user unindents the last item in a list. The list item's parent node should be
+       *       removed and this list item should be inserted in its place.
+       */
       currentPath.pop();
       // Remove old node at currentPath
       Transforms.removeNodes(editor, { at: currentPath });
       // Insert into new location
       Transforms.insertNodes(editor, currentNode, { at: insertPath });
-      // Move selection to the newly inserted node 
-      focusPath(editor, insertPath);
-    } else if (currentNodeIndex > 0 && currentNodeIndex < parentNode.children.length - 1) {
+      // Move selection to the newly inserted node
+      focusPath(editor, [...insertPath, ...finalFocusPath], focusOffset);
+    } else if (
+      currentNodeIndex > 0 &&
+      currentNodeIndex < parentNode.children.length - 1
+    ) {
       /*
-      * case: List item is in the middle of a multi-item list and a user unindents. The list should be split
-      *       rather than moving the list item below/above the current list.
-      */
+       * case: List item is in the middle of a multi-item list and a user unindents. The list should be split
+       *       rather than moving the list item below/above the current list.
+       */
       // Create lists without the current list item
       const prevList: ListElement = {
         type: parentNode.type,
-        children: parentNode.children.slice(0, currentNodeIndex)
-      }
+        children: parentNode.children.slice(0, currentNodeIndex),
+      };
       const nextList: ListElement = {
         type: parentNode.type,
-        children: parentNode.children.slice(currentNodeIndex+1, parentNode.children.length)
-      }
-      
+        children: parentNode.children.slice(
+          currentNodeIndex + 1,
+          parentNode.children.length
+        ),
+      };
+
       // Removes the whole list
       Transforms.removeNodes(editor, { at: insertPath });
 
       // Insert all nodes in reverse order
-      Transforms.insertNodes( editor, nextList, { at: insertPath });
-      Transforms.insertNodes( editor, currentNode, { at: insertPath });
-      Transforms.insertNodes( editor, prevList, { at: insertPath });
+      Transforms.insertNodes(editor, nextList, { at: insertPath });
+      Transforms.insertNodes(editor, currentNode, { at: insertPath });
+      Transforms.insertNodes(editor, prevList, { at: insertPath });
 
-      insertPath[insertPath.length-1] += 1;
+      insertPath[insertPath.length - 1] += 1;
 
-      focusPath(editor, insertPath);
-      
+      focusPath(editor, [...insertPath, ...finalFocusPath], focusOffset);
     } else if (currentNodeIndex === 0) {
       /*
-      * case: List item is at the beginning of a multi-item list and a user unindents. The list 
-      *       show move above the current list.
-      */
+       * case: List item is at the beginning of a multi-item list and a user unindents. The list
+       *       show move above the current list.
+       */
       // Remove old node at currentPath
       Transforms.removeNodes(editor, { at: currentPath });
       // Insert into new location
       Transforms.insertNodes(editor, currentNode, { at: insertPath });
-      // Move selection to the newly inserted node 
-      focusPath(editor, insertPath);
+      // Move selection to the newly inserted node
+      focusPath(editor, [...insertPath, ...finalFocusPath], focusOffset);
     } else if (currentNodeIndex === parentNode.children.length - 1) {
       /*
-      * case: List item is at the ending of a multi-item list and a user unindents. The list 
-      *       show move below the current list.
-      */
+       * case: List item is at the ending of a multi-item list and a user unindents. The list
+       *       show move below the current list.
+       */
       insertPath[insertPath.length - 1] += 1;
       // Remove old node at currentPath
       Transforms.removeNodes(editor, { at: currentPath });
       // Insert into new location
       Transforms.insertNodes(editor, currentNode, { at: insertPath });
-      // Move selection to the newly inserted node 
-      focusPath(editor, insertPath);
+      // Move selection to the newly inserted node
+      focusPath(editor, [...insertPath, ...finalFocusPath], focusOffset);
     }
   }
 };
 
 const canOutdentListItem = (editor: CustomEditor): boolean => {
-  const currentPath = isInlineActive(editor) ? getContainerPath(editor) :  getElementPath(editor);
-  if(currentPath) {
-    if(currentPath.length > 2) return true;
+  const currentPath = isInlineActive(editor)
+    ? getContainerPath(editor)
+    : getElementPath(editor);
+  if (currentPath) {
+    if (currentPath.length > 2) return true;
   }
   return false;
-}
+};
 
 const canIndentListItem = (editor: CustomEditor): boolean => {
   const activeInline = isInlineActive(editor);
-  const currentPath = activeInline ? getContainerPath(editor) :  getElementPath(editor);
-  const parentNode = activeInline ? getContainerParent(editor) : getParentElementNode(editor);
-  if(!currentPath) return false;
+  const currentPath = activeInline
+    ? getContainerPath(editor)
+    : getElementPath(editor);
+  const parentNode = activeInline
+    ? getContainerParent(editor)
+    : getParentElementNode(editor);
+  if (!currentPath) return false;
   const currentNode = getElementNode(editor, currentPath);
-  if (currentPath && currentNode && currentNode.type === 'list-item' && parentNode) {
-    const currentNodeIndex = currentPath[currentPath.length-1];
-    const nextNode = parentNode.children[currentNodeIndex+1];
+  if (
+    currentPath &&
+    currentNode &&
+    currentNode.type === "list-item" &&
+    parentNode
+  ) {
+    const currentNodeIndex = currentPath[currentPath.length - 1];
+    const nextNode = parentNode.children[currentNodeIndex + 1];
     /*
      * User can indent in the following cases:
      *   1. list item is in a multi-item list
      *   2. list item isn't the first item in a list
      *   3. list item isn't the list item before a sub-list
-    */
-    if (parentNode.children.length > 1 && currentNodeIndex !== 0 && !(currentNodeIndex === 0 && nextNode && 'type' in nextNode && isListElement(nextNode))) {
+     */
+    if (
+      parentNode.children.length > 1 &&
+      currentNodeIndex !== 0 &&
+      !(
+        currentNodeIndex === 0 &&
+        nextNode &&
+        "type" in nextNode &&
+        isListElement(nextNode)
+      )
+    ) {
       return true;
     }
   }
   return false;
-}
+};
 
 export {
   isListElement,
   indentListItem,
   canOutdentListItem,
   canIndentListItem,
-  outdentListItem
-}
+  outdentListItem,
+};
--- src/editor/toolbar/sections/lists.tsx
+++ src/editor/toolbar/sections/lists.tsx
@@ -6,61 +6,77 @@ import Indent from "../icons/indent";
 import List from "../icons/list";
 import Unindent from "../icons/unindent";
 /* -------- Actions & Types-------- */
-import { canIndentListItem, canOutdentListItem, getElementNode, getParentElementNode, indentListItem, outdentListItem, setElementType } from "../../actions";
+import {
+  canIndentListItem,
+  canOutdentListItem,
+  getElementNode,
+  getParentElementNode,
+  indentListItem,
+  outdentListItem,
+  setElementType,
+} from "../../actions";
 import { SectionContainer } from "../styled";
-import { getContainer, getContainerParent, isInlineActive } from "../../actions/inline";
+import {
+  getContainer,
+  getContainerParent,
+  isInlineActive,
+} from "../../actions/inline";
 
 const ListSection = () => {
   const editor = useSlate();
   const activeInline = isInlineActive(editor);
-  const activeElement = activeInline ? getContainer(editor) : getElementNode(editor);
-  const activeElementParent = activeInline ? getContainerParent(editor): getParentElementNode(editor);
+  const activeElement = activeInline
+    ? getContainer(editor)
+    : getElementNode(editor);
+  const activeElementParent = activeInline
+    ? getContainerParent(editor)
+    : getParentElementNode(editor);
   return (
     <SectionContainer>
       <ActionButton
-        active={activeElementParent?.type === 'ordered-list'}
+        active={activeElementParent?.type === "ordered-list"}
         onMouseDown={(event) => {
           event.preventDefault();
-          setElementType(editor, 'ordered-list');
+          setElementType(editor, "ordered-list");
         }}
       >
-        <List ordered={true} color='black' />
+        <List ordered={true} color="black" />
       </ActionButton>
       <ActionButton
-        active={activeElementParent?.type === 'bulleted-list'}
+        active={activeElementParent?.type === "bulleted-list"}
         onMouseDown={(event) => {
           event.preventDefault();
-          setElementType(editor, 'bulleted-list');
+          setElementType(editor, "bulleted-list");
         }}
       >
-        <List ordered={false} color='black' />
+        <List ordered={false} color="black" />
       </ActionButton>
-      {(activeElement?.type === 'list-item')
-        ? (
-          <>
-            {canIndentListItem(editor) ? (
-              <ActionButton
-                onMouseDown={(event) => {
-                  event.preventDefault();
-                  indentListItem(editor);
-                }}>
-                <Indent color='black' />
-              </ActionButton>
-            ) : null}
-            {canOutdentListItem(editor) ? (
-              <ActionButton
-                onMouseDown={(event: any) => {
-                  event.preventDefault();
-                  outdentListItem(editor);
-                }}
-              >
-                <Unindent color='black' />
-              </ActionButton>
-            ) : null}
-          </>
-        ) : null}
+      {activeElement?.type === "list-item" ? (
+        <>
+          {canOutdentListItem(editor) ? (
+            <ActionButton
+              onMouseDown={(event: any) => {
+                event.preventDefault();
+                outdentListItem(editor);
+              }}
+            >
+              <Unindent color="black" />
+            </ActionButton>
+          ) : null}
+          {canIndentListItem(editor) ? (
+            <ActionButton
+              onMouseDown={(event) => {
+                event.preventDefault();
+                indentListItem(editor);
+              }}
+            >
+              <Indent color="black" />
+            </ActionButton>
+          ) : null}
+        </>
+      ) : null}
     </SectionContainer>
   );
-}
+};
 
 export default ListSection;
